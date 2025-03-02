88ef1b8aba56265fe3cf113ffce8507e7fda6449
Chandra Prakash <chandra.prakash@vanshiv.com>
2025-03-02 10:23:47 +0530

    reverse a string method

---
+++ reverse-string.ts
@@ -0,0 +1,16 @@
+// Using a for loop
+function reverseMyString(str: String) {
+  let reversed: string = "";
+  for (var i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+  }
+  return reversed;
+}
+console.log(reverseMyString("Hello"));
+
+// Without using Loop
+function reverseMyStr(str: string) {
+  if (str === "") return "";
+  return reverseMyStr(str.slice(1)) + str[0];
+}
+console.log(reverseMyStr("Shivendra"));
